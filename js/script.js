7230a2ae74478af8ee9b92eddc284a671766c95f
Yurii-Shutkin <itorik@ukr.net>
2021-12-16 16:08:11 +0200

    fix some bugs

--- js/script.js
+++ js/script.js
@@ -1,23 +1,41 @@
-const getMinutes = document.querySelector('.screen-input__minutes');
-const getSeconds = document.querySelector('.screen-input__seconds');
+const minutesArea = document.querySelector('.screen-input__minutes');
+const secondsArea = document.querySelector('.screen-input__seconds');
 const runBtn = document.querySelector('.clock-face__button');
 
 const click = runBtn.addEventListener('click', () => {
+    runBtn.disabled = true;
     let timerInitValue = null;
-    
-    const startCount = function () {
-        timerInitValue = parseInt(getMinutes.value) * 60 + parseInt(getSeconds.value);
+
+    const startCount = setInterval(() => {
+        timerInitValue = parseInt(minutesArea.value) * 60 + parseInt(secondsArea.value);
         timerInitValue--;
-        let minutes = Math.floor(timerInitValue / 60 % 60);
-        let seconds = Math.floor(timerInitValue % 60);
+        getMinutes = Math.floor(timerInitValue / 60 % 60);
+        getSeconds = Math.floor(timerInitValue % 60);
+        
+        const supFormat = num => num < 10 ? num = '0' + num : num;
 
-        getMinutes.value = minutes;
-        getSeconds.value = seconds;
+        minutesArea.value = supFormat(getMinutes);
+        secondsArea.value = supFormat(getSeconds);
 
-        console.log(getMinutes.value);
-        console.log(getSeconds.value);
+        // const isValid = num => {
+        //     if(isNaN(num) || num.trim()) {
+        //         return num = '00';
+        //     }
+        //     return num;
+        // }
+
+        // secondsArea.value = (isValid(secondsArea.value));
+        // minutesArea.value = (isValid(minutesArea.value));
+
+    
+        if(timerInitValue <= 0) {
+            minutesArea.value = '00';
+            secondsArea.value = '00';
+            runBtn.disabled = false;
+            clearInterval(startCount);
+        }
+        console.log(minutesArea.value);
+        console.log(secondsArea.value);
         console.log(timerInitValue);
-    };
-    startCount()
-    setInterval(startCount, 1000);
- });
+    }, 1000);
+});
