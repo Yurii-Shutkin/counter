37d92899131a407daa17bb44f9f40acd807d6848
Yurii-Shutkin <yurashutkinn@gmail.com>
2021-12-21 01:50:56 +0200

    fixed bugs

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
+const input = Array.from(document.getElementsByTagName('input'));
 const minutesArea = document.querySelector('.screen-input__minutes');
 const secondsArea = document.querySelector('.screen-input__seconds');
 const runBtn = document.querySelector('.clock-face__button');
 
+console.log(input);
+
+const inputFocus = input.forEach(item => {
+    item.addEventListener('click', () => {
+        item.value = '';
+    })
+})
+
 const click = runBtn.addEventListener('click', () => {
     runBtn.disabled = true;
+    input.forEach(item => {
+        item.classList.add('falseTarget');
+    })
+    minutesArea.classList.add('white');
+    secondsArea.classList.add('white');
+    
+
     let timerInitValue = null;
 
     const startCount = setInterval(() => {
@@ -17,15 +33,15 @@ const click = runBtn.addEventListener('click', () => {
         minutesArea.value = supFormat(getMinutes);
         secondsArea.value = supFormat(getSeconds);
 
-        // const isValid = num => {
+        // const isValid = (num, currentValue)=> {
         //     if(isNaN(num) || num.trim()) {
-        //         return num = '00';
+        //         return num += currentValue;
         //     }
         //     return num;
         // }
 
-        // secondsArea.value = (isValid(secondsArea.value));
-        // minutesArea.value = (isValid(minutesArea.value));
+        // secondsArea.value = (isValid(secondsArea.value, timerInitValue));
+        // minutesArea.value = (isValid(minutesArea.value, timerInitValue));
 
     
         if(timerInitValue <= 0) {
@@ -34,8 +50,8 @@ const click = runBtn.addEventListener('click', () => {
             runBtn.disabled = false;
             clearInterval(startCount);
         }
-        console.log(minutesArea.value);
         console.log(secondsArea.value);
+        console.log(minutesArea.value);
         console.log(timerInitValue);
     }, 1000);
 });
