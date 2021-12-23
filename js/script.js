67203dbae421fb7659d1601298ed997e5242dd21
Yurii-Shutkin <yurashutkinn@gmail.com>
2021-12-23 09:08:46 +0200

    fixed some bugs. added max input value length

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const input = Array.from(document.getElementsByTagName('input'));
-const border = document.querySelector('.clock-face__border')
+const border = document.querySelector('.clock-face__border');
 const minutesArea = document.querySelector('.screen-input__minutes');
 const secondsArea = document.querySelector('.screen-input__seconds');
 const runBtn = document.querySelector('.clock-face__button');
@@ -8,28 +8,32 @@ console.log(input);
 
 const inputFocus = input.forEach(item => {
     item.addEventListener('click', () => {
-        item.value = '';
         border.classList.add('green');
-    })
-})
+        item.oninput = function() {
+            this.value = this.value.substr(0, 2);
+        };
+        item.value = '';
+    });
+});
 
 const click = runBtn.addEventListener('click', () => {
-    let timerInitValue = null;
     border.classList.add('red');
     runBtn.disabled = true;
     input.forEach(item => {
         item.classList.add('falseTarget');
         item.classList.add('white');
-    })
-
+        if(item.value === '') item.value = item.placeholder;
+        if(item.value.length === 1) item.value = '0' + item.value; 
+        if(item.value > item.max) item.value = item.max;
+    });    
+        
     const startCount = setInterval(() => {
-        timerInitValue = parseInt(minutesArea.value) * 60 + parseInt(secondsArea.value);
+        let timerInitValue = parseInt(minutesArea.value) * 60 + parseInt(secondsArea.value);
         timerInitValue--;
         getMinutes = Math.floor(timerInitValue / 60 % 60);
         getSeconds = Math.floor(timerInitValue % 60);
         
         const supFormat = num => num < 10 ? num = '0' + num : num;
-
         minutesArea.value = supFormat(getMinutes);
         secondsArea.value = supFormat(getSeconds);
     
@@ -37,12 +41,14 @@ const click = runBtn.addEventListener('click', () => {
             input.forEach(item => {
                 item.classList.remove('falseTarget');
                 item.classList.remove('white');
+                item.value = '';
             })
             border.classList.remove('green');
             border.classList.remove('red');
             runBtn.disabled = false;
             clearInterval(startCount);
         }
+
         console.log(secondsArea.value);
         console.log(minutesArea.value);
         console.log(timerInitValue);
